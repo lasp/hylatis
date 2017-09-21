f47a2600c5c055b366a82b9afe4948725ae936d6
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2017-09-21 17:21:38 -0600

    Comment out some stuff until we can port it to v3

--- src/main/scala/latis/server/LatisHylatisServer.scala
+++ src/main/scala/latis/server/LatisHylatisServer.scala
@@ -9,42 +9,41 @@ import org.eclipse.jetty.servlet.ServletContextHandler
 
 import latis.reader.DatasetSource
 import latis.writer.SparkDataFrameWriter
-import latis.writer.Writer3
 
 class LatisHylatisServer extends HttpServlet {
 
   override def init(): Unit = {
-    loadData("ascii")
+ //   loadData("ascii")
   }
 
   override def doGet(
     request: HttpServletRequest,
     response: HttpServletResponse
-  ): Unit = {
-    // Making the assumption that the request is just the name of the
-    // dataset without any suffix or query.
-    val name = {
-      val path = request.getPathInfo
-      // Drop the leading "/"
-      path.drop(1)
-    }
-
-    val ds = DatasetSource.fromName(name).getDataset()
-
-    val writer = {
-      val os = response.getOutputStream
-      Writer3(os)
-    }
-    writer.write(ds)
-
-    response.setStatus(HttpServletResponse.SC_OK)
-    response.flushBuffer()
-  }
-
-  private def loadData(name: String): Unit = {
-    val ds = DatasetSource.fromName(name).getDataset()
-    SparkDataFrameWriter.write(ds)
-  }
+  ): Unit = {}
+//    // Making the assumption that the request is just the name of the
+//    // dataset without any suffix or query.
+//    val name = {
+//      val path = request.getPathInfo
+//      // Drop the leading "/"
+//      path.drop(1)
+//    }
+//
+//    val ds = DatasetSource.fromName(name).getDataset()
+//
+//    val writer = {
+//      val os = response.getOutputStream
+//      Writer3(os)
+//    }
+//    writer.write(ds)
+//
+//    response.setStatus(HttpServletResponse.SC_OK)
+//    response.flushBuffer()
+//  }
+//
+//  private def loadData(name: String): Unit = {
+//    val ds = DatasetSource.fromName(name).getDataset()
+//    SparkDataFrameWriter.write(ds)
+//  }
 }
 
 object LatisHylatisServer {

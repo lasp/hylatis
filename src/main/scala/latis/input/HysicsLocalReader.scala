e76d91cd4e70b66980be84c4faf3ff83b7024a50
Doug Lindholm <doug.lindholm@gmail.com>
2018-05-15 16:33:38 -0600

    snapshot - hysics image sorting

--- src/main/scala/latis/input/HysicsLocalReader.scala
+++ src/main/scala/latis/input/HysicsLocalReader.scala
@@ -11,22 +11,22 @@ import latis.util.HysicsUtils
 import latis.util.AWSUtils
 import java.net.URI
 
-class HysicsLocalReader(dir: String) extends DatasetSource {
+class HysicsLocalReader(uri: URI) extends DatasetSource {
   //TODO: make a Matrix subtype of SampledFunction with matrix semantics
   //TODO: allow value to be any Variable type?
   //TODO: impl as Adapter so we can hand it a model with metadata
   //TODO: optimize with index logic
   
   val granuleListDataset: Dataset = {
-    val yType = ScalarType("y")
+    val yType = ScalarType("iy")
     val gType = ScalarType("uri")
     val model = FunctionType("")(yType, gType)
     
     val md = Metadata("id" -> "image_files")(model)
     
-    val base = "s3:/hylatis-hysics-001/des_veg_cloud"
+    val base = uri.toString //"s3:/hylatis-hysics-001/des_veg_cloud"
  //   val samples = Iterator.range(1, 4201) map { i =>
-    val samples = Iterator.range(1, 11) map { i =>
+val samples = Iterator.range(1, 51) map { i =>
       val y = Integer(i)
       val uri = Text(f"${base}/img$i%04d.txt")
       Sample(y, uri)
@@ -34,17 +34,16 @@ class HysicsLocalReader(dir: String) extends DatasetSource {
     
     Dataset(md, Function.fromSamples(samples))
   }
-  
-  val delimiter = "," //TODO: parameterize
 
-  val model = {
-    val xType = ScalarType("x")
-    val yType = ScalarType("y")
-    val wavelength = ScalarType("wavelength")
-    val domain = TupleType("")(xType, yType, wavelength)
-    val range = ScalarType("value")
-    FunctionType("f")(domain, range)
-  }
+  // (y, x, wavelength) -> irradiance
+  val model = FunctionType("f")(
+    TupleType("")(
+      ScalarType("y"),
+      ScalarType("x"),
+      ScalarType("wavelength")
+    ),
+    ScalarType("irradiance")
+  )
   
   val metadata = Metadata("id" -> "hysics")(model)
   
@@ -84,57 +83,56 @@ class HysicsLocalReader(dir: String) extends DatasetSource {
    */
   
   private lazy val wavelengths: Array[Double] = {
-    val source = Source.fromFile(dir + "wavelength.txt")
+    //TODO: make wavelength dataset
+    val wuri = URI.create(s"${uri.toString}/wavelength.txt")
+    val is = URIResolver.resolve(wuri).getStream
+    val source = Source.fromInputStream(is)
     val data = source.getLines().next.split(",").map(_.toDouble)
     source.close
     data
   }
-   
-  private def readImage(uri: URI): Array[Array[Double]] = {
-    val path = uri.getPath.drop(1)  // drop leading "/"
-    // Split bucker name from object name at first "/".
-    val (bucket, file) = path.splitAt(path.indexOf('/')) match {
-      case (b, f) => (b, f.drop(1))
-    }
-    
-    val s3 = AWSUtils.s3Client.get
-    val is = s3.getObject(bucket, file).getObjectContent
-    val source = Source.fromInputStream(is)
-    val data = source.getLines.map(_.split(delimiter).map(_.toDouble)).toArray
-    source.close
-    data
-  }
+
   
   /**
    * Read slit images (x,w) -> f
    * TODO: add lon, lat
-   * then logically join on a new axis "y"
+   * then join on a new axis "y"
+   *   iy -> (ix, iw) -> f
+   * transform into x/y space and add wavelengths
+   * uncurry but don't transpose (order implications)
+   *   (y, x, w) -> f
    */
   def getDataset(operations: Seq[Operation]): Dataset = {
-    val uris = granuleListDataset.samples map {
-      case Sample(_, Seq(_, Text(uri))) => new URI(uri)
+val xops = List(Select("row < 50"))
+    val samples: Iterator[Sample] = granuleListDataset.samples.zipWithIndex flatMap {
+      case (Sample(_, Seq(_, Text(uri))), iy) => 
+        MatrixTextReader(new URI(uri)).getDataset(xops).samples map {
+          case Sample(_, Seq(Index(ix), Index(iw), Text(v))) =>
+            val (x, y) = HysicsUtils.indexToXY((ix, iy))
+            Sample(3, Real(y), Real(x), Real(wavelengths(iw)), Real(v.toDouble))
+        }
     }
     
-    val samples: Iterator[Sample] = uris.zipWithIndex.flatMap { case (uri, iy) =>
-      val image: Array[Array[Double]] = readImage(uri)
-
-      val nx = image.length
-      val nw = wavelengths.length
-
-      for (
-        ix <- Iterator.range(0, nx);
-        iw <- Iterator.range(0, nw)
-      ) yield {
-        //val ll = HysicsUtils.indexToGeo(ix, iy)
-        val ll = HysicsUtils.indexToXY(ix, iy) //leave in native Cartesian x/y space
-        val lon = Scalar(ll._1)
-        val lat = Scalar(ll._2)
-        val w = Scalar(wavelengths(iw))
-        val value = Scalar(image(ix)(iw).toDouble)
-        val domain = Tuple(lon, lat, w)
-        Sample(domain, value)
-      }
-    }
+//    val samples: Iterator[Sample] = uris.zipWithIndex.flatMap { case (uri, iy) =>
+//      val image: Dataset = MatrixTextReader(uri).getDataset()
+//
+//      val nx = image.length
+//      val nw = wavelengths.length
+//
+//      for (
+//        ix <- Iterator.range(0, nx);
+//        iw <- Iterator.range(0, nw)
+//      ) yield {
+//        //val ll = HysicsUtils.indexToGeo(ix, iy)
+//        val ll = HysicsUtils.indexToXY(ix, iy) //leave in native Cartesian x/y space
+//        val lon = Scalar(ll._1)
+//        val lat = Scalar(ll._2)
+//        val w = Scalar(wavelengths(iw))
+//        val value = Scalar(image(ix)(iw).toDouble)
+//        val domain = Tuple(lon, lat, w)
+//        Sample(domain, value)
+//      }
+//    }
 
     val dataset = Dataset(metadata, Function.fromSamples(samples))
     operations.foldLeft(dataset)((ds, op) => op(ds))
@@ -143,9 +141,10 @@ class HysicsLocalReader(dir: String) extends DatasetSource {
 
 object HysicsLocalReader {
   
-  def apply() = new HysicsLocalReader("/data/hysics/des_veg_cloud/")
+  //def apply() = new HysicsLocalReader("/data/hysics/des_veg_cloud/")
+  def apply() = new HysicsLocalReader(URI.create("s3:/hylatis-hysics-001/des_veg_cloud"))
 
-  def apply(dir: String): HysicsLocalReader = {
-    new HysicsLocalReader(dir)
+  def apply(s3Base: String): HysicsLocalReader = {
+    new HysicsLocalReader(URI.create(s3Base))
   }
 }

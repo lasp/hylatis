828171a2582acad495a31064a21ba8febcd2d2af
Doug Lindholm <dlindhol@gmail.com>
2019-10-22 10:21:08 -0500

    Migrate incomplete IndexedFunctions from latis3-core

---
+++ src/main/scala/latis/data/IndexedFunction1D.scala
@@ -0,0 +1,32 @@
+package latis.data
+
+import scala.collection.Searching._
+
+/**
+ * Manage one-dimensional SampledFunction Data as columnar sequences.
+ * For evaluation, this uses a binary search on the domain values
+ * to get the index into the range values.
+ */
+case class IndexedFunction1D(xs: Seq[Data], vs: Seq[RangeData]) extends IndexedFunction {
+  //TODO: assert that sizes are consistent
+  //Note, using Seq instead of invariant Array to get variance
+  //TODO: prevent diff types of OrderedData, e.g. mixing NumberData and TextData, or IntData and DoubleData
+  // [T <: OrderedData]
+  
+  override def apply(
+    dd: DomainData
+  ): Option[RangeData] = dd match {
+    case DomainData(d) =>
+      searchDomain(xs, d) match {
+        case Found(i) => Option(vs(i))
+        case InsertionPoint(i) => ??? //TODO: interpolate
+      }
+  }
+  
+  /**
+   * Provide a sequence of samples to fulfill the MemoizedFunction trait.
+   */
+  def samples: Seq[Sample] =
+    (xs zip vs) map { case (x, v) => Sample(DomainData(x), v) }
+
+}
---
+++ src/main/scala/latis/data/IndexedFunction2D.scala
@@ -0,0 +1,35 @@
+package latis.data
+
+import scala.collection.Searching._
+
+/**
+ * Manage two-dimensional Cartesian SampledFunction Data as columnar arrays.
+ * The Cartesian nature allows the domain values for each dimension to be
+ * managed separately.
+ * For evaluation, this uses a binary search on the domain values
+ * to get the indices into the range values.
+ */
+case class IndexedFunction2D(xs: Seq[Data], ys: Seq[Data], vs: Seq[Seq[RangeData]]) extends IndexedFunction {
+  //TODO: assert that sizes are consistent
+  
+  override def apply(
+    dd: DomainData
+  ): Option[RangeData] = dd match {
+    case DomainData(x, y) =>
+      (searchDomain(xs, x), searchDomain(ys, y)) match {
+        case (Found(i), Found(j)) => Option(vs(i)(j))
+        case _ => ??? //TODO: interpolate
+      }
+    case _ => ??? //TODO: error, invalid type
+  }
+  
+  /**
+   * Provide a sequence of samples to fulfill the MemoizedFunction trait.
+   */
+  def samples: Seq[Sample] = {
+    for {
+      ia <- 0 until xs.length
+      ib <- 0 until ys.length
+    } yield Sample(DomainData(xs(ia), ys(ib)), vs(ia)(ib))
+  }
+}
